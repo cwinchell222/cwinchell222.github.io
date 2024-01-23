0359f476894196e641afa76563341238f1a6ec31
Cody Winchell <cwinchell222@gmail.com>
2024-01-23 12:52:30 -0500

    Restest Shell Script

--- git-push.sh
+++ git-push.sh
@@ -6,4 +6,4 @@ read commitComment
 git commit -m "$commitComment"
 echo 'What branch should the commit be pushed to?'
 read branch
-git push origin "$branch"%                                            
+git push origin "$branch"                                            
