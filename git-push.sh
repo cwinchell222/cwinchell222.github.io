3da0c2bca77eba47eb95f6b19bbf6f135761f74f
Cody Winchell <cwinchell222@gmail.com>
2024-01-23 12:51:38 -0500

    testing shell script

---
+++ git-push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git status
+git add .
+echo 'Enter a commit comment: '
+read commitComment
+git commit -m "$commitComment"
+echo 'What branch should the commit be pushed to?'
+read branch
+git push origin "$branch"%                                            
